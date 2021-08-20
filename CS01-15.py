2906be9535e4dc879bd429731cdc55562c447629
Dawn Suwanpratet <85325505+DawnSuwanPratet@users.noreply.github.com>
2021-08-20 09:25:01 +0700

    function update

--- CS01-15.py
+++ CS01-15.py
@@ -1,1 +1,10 @@
-def 
+def my_list(num):
+    for i in range(len(num)):
+        if num[i] == 20:
+            num[i] = 200
+    
+    print(num)
+
+my_num = [5, 10, 20, 25, 50, 20, 15, 20]
+
+my_list(my_num)
