23ad1ed112622cc2720c5d1462d6099ccdcc423d
Dawn Suwanpratet <85325505+DawnSuwanPratet@users.noreply.github.com>
2021-09-06 17:30:40 +0700

    ืnumpy update

--- CS01-16.py
+++ CS01-16.py
@@ -2,6 +2,7 @@ import numpy as np
 from numpy import random
 
 arr = random.randint(100, size = (5, 10))
-newarr = np.sort(arr)
+newarr = np.sort(arr %5 == 0)
+overnewarr = np.sort(newarr)
 
-print(newarr)
+print(overnewarr)
